0bbea9166781fc1de11819f69c3de0f4147e345d
Jaiver Andres <jaiverandres98@hotmail.com>
2020-04-12 00:30:02 -0500

    solo una cosita

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    </v-app-bar>
+
+    <v-content>
+      <Login/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Login from './components/Login';
+
+export default {
+  name: 'App',
+
+  components: {
+    Login,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
