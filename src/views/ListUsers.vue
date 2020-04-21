f1396da5c23c555f315c9e3f2e06a35b7da2ecda
Jaiver Andres <jaiverandres98@hotmail.com>
2020-04-20 21:46:51 -0500

    s

--- src/views/ListUsers.vue
+++ src/views/ListUsers.vue
@@ -5,7 +5,6 @@
         <v-toolbar-title>Usuarios</v-toolbar-title>
         <v-divider class="mx-4" inset vertical></v-divider>
         <v-spacer>
-            <v-btn color="blue" text @click="add">Agregar usuario</v-btn>
         </v-spacer>
         <v-dialog v-model="dialog" max-width="500px">
           <template v-slot:activator="{ }">
@@ -18,89 +17,10 @@
               single-line
               hide-details
             ></v-text-field>
-
           </template>
-            
-        
-          <v-card>
-            <v-card-title>
-              <span class="headline">{{ formTitle }}</span>
-            </v-card-title>
-            <h1></h1>
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.name" :rules="nameRules" label="Nombre"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.lastName" :rules="lastNameRules" label="Apellido"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.email" :rules="emailRules" label="Email"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.role" :rules="roleRules" label="Rol"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.password" :rules="passwordRules" label="Contraseña" type="password"></v-text-field>
-                  </v-col>
-                   <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.state" :rules="stateRules" label="Estado"></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer>
-                  
-              </v-spacer>
-              <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-              <v-btn color="blue darken-1" text @click="save">Save</v-btn>
-            </v-card-actions>
-          </v-card>
         </v-dialog>
-        
       </v-toolbar>
-      <v-dialog v-model="showDismissibleAlert" max-width="290">
-         <v-card>
-        <v-card-title class="headline">Lo sentimos!</v-card-title>
-
-        <v-card-text>
-          {{error}}
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-
-          <v-btn
-            color="green darken-1"
-            text
-            @click="showDismissibleAlert = false"
-          >
-            Aceptar
-          </v-btn>
-        </v-card-actions>
-         </v-card>
-      </v-dialog>
     </template>
-    <template v-slot:item.action="{ item }">
-      <div class="my-2">
-        <div class="my-2">
-        <v-btn small color="success" @click="editItem(item)" >Editar</v-btn>
-        </div>
-
-        <div class="my-2">
-        <v-btn small color="red" @click="deleteItem(item)" >Eliminar</v-btn>
-        </div>
-        
-      </div>
-    </template>
-    <template v-slot:no-data>
-      <v-btn color="primary" @click="initialize">Reset</v-btn>
-    </template>
-
-
   </v-data-table>
 </template>
 
@@ -170,9 +90,6 @@ export default {
     },
    async initialize() {
         this.desserts=[];
-        
-        //console.log(this.numberMessage());
-       // console.log(this.numberMessage());
      await db.collection("users")
         .get()
         .then((querySnapshot) => {
@@ -188,7 +105,7 @@ export default {
                 'state':doc.data().state,
 
             }
-            //this.numberMessage("admin2@prueba.com");
+      
              console.log(this.number);
             this.desserts.push(data);
           });
@@ -198,9 +115,7 @@ export default {
 
 
     },
-
   },
-
    created() {
      this.initialize();
   },
